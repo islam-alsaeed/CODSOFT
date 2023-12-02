8a3379c32b0d8ebdf72ef1c226771a283460adc9
Sam <islam.alsaeed@gmail.com>
2023-12-02 16:51:11 +0200

    add signup logic

--- backend/controllers/authiController.js
+++ backend/controllers/authiController.js
@@ -1,3 +1,19 @@
-exports.signin=(req,res)=>{
-    res.send("Hello in node js sign in")
+const User = require('../models/userModel');
+const errorResponse = require('../utils/errorResponse')
+
+exports.signup = async (req, res, next) => {
+    const { email } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        return next(new errorResponse('Email already registred', 400));
+    }
+    try {
+        const user = User.create(req.body);
+        res.status(201).json({
+            success: true,
+            user
+        })
+    } catch (error) {
+        next(error);
+    }
 }
--- backend/routes/authiRoute.js
+++ backend/routes/authiRoute.js
@@ -1,7 +1,7 @@
 const express=require("express");
-const { signin } = require("../controllers/authiController");
+const { signup } = require("../controllers/authiController");
 const router=express.Router();
 
-// 
-router.get('/',signin);
+// signup page
+router.post('/signup',signup)
 module.exports=router;
