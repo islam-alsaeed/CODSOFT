58543411f927e68cc31afca7f2fcc3e6c55f2690
Sam <islam.alsaeed@gmail.com>
2023-12-05 11:14:56 +0200

    begin the front end

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
