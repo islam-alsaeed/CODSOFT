f312e11f0a2a53b3a0f4ee49b47a88d3f2b98661
Sam <islam.alsaeed@gmail.com>
2023-12-05 23:52:04 +0200

    add nav bar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Navbar from "../component/Navbar";
 const Home = () => {
     return (
         <>
+        <Navbar/>
             <h1>Home page</h1>
         </>
     )
