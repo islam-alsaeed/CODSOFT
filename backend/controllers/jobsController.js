34a8a9c0b1c78412ac5f11b032af6bdf6221824d
Sam <islam.alsaeed@gmail.com>
2023-12-07 19:49:35 +0200

    edit job functionality

--- backend/controllers/jobsController.js
+++ backend/controllers/jobsController.js
@@ -1,20 +1,20 @@
-const Job=require('../models/jobModel');
+const Job = require('../models/jobModel');
 const errorResponse = require('../utils/errorResponse');
 
 // job creation 
-exports.createJob= async(req,res,next)=>{
+exports.createJob = async (req, res, next) => {
     try {
-        
-        const job =await Job.create({
-            title:req.body.title,
-            description:req.body.description,
-            location:req.body.location,
-            salary:req.body.salary,
-            jobType:req.body.jobType,
-            user:req.user.id,
+
+        const job = await Job.create({
+            title: req.body.title,
+            description: req.body.description,
+            location: req.body.location,
+            salary: req.body.salary,
+            jobType: req.body.jobType,
+            user: req.user.id,
         });
         res.status(201).json({
-            succuss: true, 
+            succuss: true,
             job
         })
     } catch (error) {
@@ -23,13 +23,27 @@ exports.createJob= async(req,res,next)=>{
 }
 
 // display one job 
-exports.OneJob= async(req,res,next)=>{
+exports.OneJob = async (req, res, next) => {
     try {
-        
-        const job =await Job.findById(req.param.id);
+
+        const job = await Job.findById(req.params.id);
         res.status(200).json({
-            succuss: true, 
-            job_type
+            succuss: true,
+            job
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+// edit job
+exports.editJob = async (req, res, next) => {
+    try {
+
+        const job = await Job.findByIdAndUpdate(req.params.job_id, req.body, { new: true }).populate('jobType','JobTypeName').populate('user','firstName lastName');
+        res.status(200).json({
+            succuss: true,
+            job
         })
     } catch (error) {
         next(error);
--- backend/routes/jobsRoutes.js
+++ backend/routes/jobsRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const { ifAuthenticated } = require('../middleware/authentication ');
-const { createJob, OneJob } = require('../controllers/jobsController');
+const { ifAuthenticated, recruiter } = require('../middleware/authentication ');
+const { createJob, OneJob, editJob } = require('../controllers/jobsController');
 const router = express.Router();
 
 // job  routes
 
 // Job creation route
-router.post('/job/create',ifAuthenticated,createJob)
+router.post('/job/create',ifAuthenticated,recruiter,createJob);
+
+// edit job route
+router.put('/job/edit/:job_id',ifAuthenticated,recruiter,editJob);
 
 // display one job route
-router.get('/job/:id',OneJob)
+router.get('/job/:id',OneJob);
+
 
 module.exports = router;
