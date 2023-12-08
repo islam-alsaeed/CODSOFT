79af0aecd85e7f2a9a0f9dd0d41dc481a52aed38
Sam <islam.alsaeed@gmail.com>
2023-12-08 16:47:13 +0200

    edit job type logic

--- backend/controllers/jobTypeController.js
+++ backend/controllers/jobTypeController.js
@@ -1,16 +1,16 @@
-const JobType=require('../models/jobTypeModel');
+const JobType = require('../models/jobTypeModel');
 const errorResponse = require('../utils/errorResponse');
 
 // job category creation 
-exports.createJobType= async(req,res,next)=>{
+exports.createJobType = async (req, res, next) => {
     try {
-        
-        const job_type =await JobType.create({
-            JobTypeName:req.body.JobTypeName,
-            user:req.user.id,
+
+        const job_type = await JobType.create({
+            JobTypeName: req.body.JobTypeName,
+            user: req.user.id,
         });
         res.status(201).json({
-            succuss: true, 
+            succuss: true,
             job_type
         })
     } catch (error) {
@@ -19,12 +19,26 @@ exports.createJobType= async(req,res,next)=>{
 }
 
 // display all job types 
-exports.displayAllJobType= async(req,res,next)=>{
+exports.displayAllJobType = async (req, res, next) => {
     try {
-        
-        const job_type =await JobType.find();
+
+        const job_type = await JobType.find();
         res.status(200).json({
-            succuss: true, 
+            succuss: true,
+            job_type
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+// update job type
+exports.updateJobType = async (req, res, next) => {
+    try {
+
+        const job_type = await JobType.findByIdAndUpdate(req.params.type_id, req.body, {new: true});
+        res.status(200).json({
+            succuss: true,
             job_type
         })
     } catch (error) {
--- backend/routes/jobTypeRoutes.js
+++ backend/routes/jobTypeRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { createJobType, displayAllJobType } = require('../controllers/jobTypeController');
-const { ifAuthenticated } = require('../middleware/authentication ');
+const { createJobType, displayAllJobType, updateJobType } = require('../controllers/jobTypeController');
+const { ifAuthenticated, recruiter } = require('../middleware/authentication ');
 const router = express.Router();
 
 
 // job type routes
-router.post('/jobtype/create',ifAuthenticated,createJobType)
+router.post('/jobtype/create',ifAuthenticated,recruiter,createJobType)
+// display all job types
 router.get('/jobtype/jobs',displayAllJobType)
+// edit job type
+router.put('/jobtype/edit/:type_id',ifAuthenticated,recruiter,updateJobType)
 
 
 
